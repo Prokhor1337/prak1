e7f53b5534c8f3a5a2804c1117c6eb65509e24f8
Prokhor1337 <chertilin2006@gmail.com>
2025-03-27 08:44:19 +0000

    Update zav3.py

--- prak5/zav3.py
+++ prak5/zav3.py
@@ -1,1 +1,8 @@
+age= int(input("Введіть свій вік:"))
+year_exp= int(input("Введіть кількість років досвіду:"))
+ed= str(input("Введіть чи є у вас освіта(true/false):")).strip().lower() == "true"
 
+if (age<=50 and age>=25) and (year_exp>=3 or ed):
+    print("вас прийнято")
+else:
+    print("ти в пролете")
